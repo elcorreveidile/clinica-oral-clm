5853b960a46652dcda76b0ccd03a11d7d8813aa7
Javier Benítez Láinez <benitezl@go.ugr.es>
2026-02-12 03:05:29 +0100

    Fix: Disable ESLint rule for unused vars in signin page

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
